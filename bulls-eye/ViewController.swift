d0e86263f29a1f37d1f25f10135b87bfa9473aeb
Arsalan Wahid Asghar <arsalanwahid1993@gmail.com>
2018-06-28 14:21:11 +0500

    Added Logic to update labels

--- bulls-eye/ViewController.swift
+++ bulls-eye/ViewController.swift
@@ -12,16 +12,25 @@ class ViewController: UIViewController {
     
     //MARK:- OUTLETS
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targatValueLabel: UILabel!
+    @IBOutlet weak var totalScoreTextLabel: UILabel!
+    
+    @IBOutlet weak var roundtextLabel: UILabel!
+    
+    
     
     //MARK:- Properties
     var currentValue = 0
     var targetValue = 0
     var round = 0
+    var totalScore = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startNewRound()
+        UpdateLabels()
     
     }
     
@@ -30,20 +39,26 @@ class ViewController: UIViewController {
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
+        round += 1
+        
     }
     
+    func UpdateLabels(){
+        targatValueLabel.text = String(targetValue)
+        totalScoreTextLabel.text = String(totalScore)
+        roundtextLabel.text = String(round)
+    }
     
-    
-    
-    
-    
-    
-    
-    
+    func calcualteScore(){
+    }
+
     //MARK:- Actions
     @IBAction func showAlert() {
         
-        let alertMessage = "The current value of the slider is \(currentValue)\nThe target value is \(targetValue) "
+        let difference = abs(currentValue - targetValue)
+        let points = 100 - difference
+        totalScore += points
+        let alertMessage = "You scored \(points) points"
         
         //write logic to to show alert view here
         let alert = UIAlertController(title:"Hello World", message: alertMessage, preferredStyle: .alert)
@@ -58,15 +73,21 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
         
         startNewRound()
+        UpdateLabels()
         
     }
     
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         
-        //When the slider moves set the slider value
+        //When the slider moves set the slider value ,slider returns a float
         currentValue = lround(Double(slider.value))
         
+    }
+    
+    @IBAction func startOver(_ sender: UIButton) {
+        totalScore = 0
+        round = 0
     }
     
     
